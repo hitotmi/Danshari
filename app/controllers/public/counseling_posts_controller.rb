ce350806cde92ca92b5f87bc2ad01520df57e12f
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-06-03 14:19:09 +0000

    [Add]public/counseling_postの一覧画面

--- app/controllers/public/counseling_posts_controller.rb
+++ app/controllers/public/counseling_posts_controller.rb
@@ -1,2 +1,34 @@
 class Public::CounselingPostsController < ApplicationController
+
+  def new
+    @counseling_post = CounselingPost.new
+  end
+
+  def create
+    @counseling_post = CounselingPost.new(counseling_post_params)
+    @counseling_post.user_id = current_user.id
+    if  @counseling_post.save
+      redirect_to counseling_posts_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @counseling_posts = CounselingPost.all.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destory
+  end
+
+  def counseling_post_params
+    params.require(:counseling_post).permit(:title, :content, :status, :image, :usage_frequency, :star)
+  end
+
 end
--- app/models/counseling_post.rb
+++ app/models/counseling_post.rb
@@ -1,2 +1,23 @@
 class CounselingPost < ApplicationRecord
+
+  has_one_attached :image
+  belongs_to :user
+
+
+  validates :title, presence:true
+  validates :content, length: { maximum: 200 }, presence:true
+  validates :status, presence:true
+
+  enum status: { answer_reception: "回答受付中", resolved: "解決済" }
+  enum usage_frequency: { everyday: "毎日", weeks_once: "週に1回", month_once: "月に1回", once_to_half_year: "半年に1回", once_to_1_year: "1年に1回", unused: "ほとんど使っていない" }
+
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+
 end
