6213083b4d7afc91d7bb2865ce2d005880b54381
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-06-04 01:05:30 +0000

    [Add]public/usersの詳細画面

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,2 +1,32 @@
 class Public::UsersController < ApplicationController
+
+  def show
+     @user = User.find(params[:id])
+     @counseling_posts = @user.counseling_posts
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+  def withdraw_confirm
+  end
+
+  def withdraw
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction, :is_deleted)
+  end
+
+
 end
