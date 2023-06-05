72dffe5c0dcae615465ca9041eaabaad26827347
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-06-05 07:03:28 +0000

    [Add]管理者側の会員編集画面作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,4 +8,22 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "会員情報を編集しました。"
+      redirect_to admin_user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :email, :is_deleted, :admin)
+  end
+
 end
