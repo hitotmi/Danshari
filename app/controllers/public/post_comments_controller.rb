521006df79fdc4be63b7e4f9def7067a9bc8f99e
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-06-06 09:55:53 +0000

    [Add]コメントの非同期通信化

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,18 +1,19 @@
 class Public::PostCommentsController < ApplicationController
 
   def create
-    counseling_post = CounselingPost.find(params[:counseling_post_id])
+    @counseling_post = CounselingPost.find(params[:counseling_post_id])
     comment = current_user.post_comments.new(post_comment_params)
-    comment.counseling_post_id = counseling_post.id
+    comment.counseling_post_id = @counseling_post.id
     comment.save
-    redirect_to request.referer
+
     @post_comment_item = comment.counseling_post
     @post_comment_item.create_notification_post_comment!(current_user, comment.id)
   end
 
   def destroy
-    PostComment.find(params[:id]).destroy
-    redirect_to request.referer
+    @counseling_post = CounselingPost.find(params[:counseling_post_id])
+    comment = PostComment.find(params[:id])
+    comment.destroy
   end
 
   def post_comment_params
