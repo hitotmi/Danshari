e5b82a1773fd472aff0431c154d3e9b13be908c4
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-06-06 11:30:33 +0000

    [Fix]既読ボタン削除

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -2,12 +2,9 @@ class Public::NotificationsController < ApplicationController
 
   def index
     @notifications = current_user.passive_notifications.order(created_at: :desc)
-  end
-
-  def mark_read
-    notification = Notification.find(params[:id])
-    notification.update(checked: true)
-    redirect_to notifications_path
+    @notifications.where(checked: false).each do |notification|
+      notification.update(checked: true)
+    end
   end
 
   def destroy
