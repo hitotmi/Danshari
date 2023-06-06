66a107c2b37f2f7841bc5df609039aab85397f1b
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-06-06 09:09:40 +0000

    [Add]いいねの非同期通信化

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,18 +1,16 @@
 class Public::FavoritesController < ApplicationController
 
   def create
-    counseling_post = CounselingPost.find(params[:counseling_post_id])
-    favorite = current_user.favorites.new(counseling_post_id: counseling_post.id)
+    @counseling_post = CounselingPost.find(params[:counseling_post_id])
+    favorite = current_user.favorites.new(counseling_post_id: @counseling_post.id)
     favorite.save
-    counseling_post.create_notification_by(current_user, counseling_post.id, counseling_post.user_id)
-    redirect_to request.referer
+    @counseling_post.create_notification_by(current_user, @counseling_post.id, @counseling_post.user_id)
   end
 
   def destroy
-    counseling_post = CounselingPost.find(params[:counseling_post_id])
-    favorite = current_user.favorites.find_by(counseling_post_id: counseling_post.id)
+    @counseling_post = CounselingPost.find(params[:counseling_post_id])
+    favorite = current_user.favorites.find_by(counseling_post_id: @counseling_post.id)
     favorite.destroy
-    redirect_to request.referer
   end
 
 end
--- app/helpers/public/notifications_helper.rb
+++ app/helpers/public/notifications_helper.rb
@@ -3,7 +3,6 @@ module Public::NotificationsHelper
   def notification_form(notification)
     @visitor = notification.visitor
     @visitor_comment = notification.post_comment_id
-    # @post_comment = PostComment.find_by(id: @visitor_comment)&.comment
 
     case notification.action
       when "post_comment"
@@ -13,8 +12,8 @@ module Public::NotificationsHelper
         your_counseling_post = link_to 'あなたの投稿', counseling_post_path(notification.counseling_post_id), style: "font-weight: bold;"
         tag.a(notification.visitor.name, href: user_path(@visitor), style: "font-weight: bold;") + "さんが" + your_counseling_post + "を参考にしました"
       when "good_comment"
-        your_counseling_post = link_to 'あなた回答', counseling_post_path(notification.post_comment.counseling_post_id), style: "font-weight: bold;"
-        tag.a(@visitor.name, href: user_path(@visitor), style: "font-weight: bold;") + "さんが" + your_counseling_post + "役立ったったと評価しました"
+        your_counseling_post = link_to 'あなたの回答', counseling_post_path(notification.post_comment.counseling_post_id), style: "font-weight: bold;"
+        tag.a(@visitor.name, href: user_path(@visitor), style: "font-weight: bold;") + "さんが" + your_counseling_post + "が役に立ったと評価しました"
     end
   end
 
