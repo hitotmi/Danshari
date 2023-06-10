be223cee17628947ed03409cb91379e70f5e1c38
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-06-10 10:21:15 +0000

    [Fix]コメント投稿時のフラッシュメッセージ修正

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -8,7 +8,7 @@ class Public::PostCommentsController < ApplicationController
     unless @post_comment.save
       render 'error' # app/views/public/post_comments/error.js.erbを参照する
     else
-      flash.now[:notice] = 'コメントを投稿しました'
+      flash.now[:post_comment_created] = "回答が作成されました。"
       @post_comment_item = @post_comment.counseling_post
       @post_comment_item.create_notification_post_comment!(current_user, @post_comment.id)
       @post_comments = @counseling_post.post_comments.order(created_at: :desc).page(params[:page]).per(10)
@@ -19,7 +19,7 @@ class Public::PostCommentsController < ApplicationController
     @counseling_post = CounselingPost.find(params[:counseling_post_id])
     comment = PostComment.find_by(id: params[:id])
     comment.destroy
-    flash.now[:notice] = '投稿を削除しました'
+    flash.now[:post_comment_created] = '回答を削除しました'
     @post_comments = @counseling_post.post_comments.order(created_at: :desc).page(params[:page]).per(10)
   end
 
--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,3 +1,4 @@
+// 投稿新規・編集の画像表示
 $(document).on('turbolinks:load', function() {
   $("#image-preview").hide();
 
@@ -18,5 +19,32 @@ $(document).on('turbolinks:load', function() {
   }
 
   var fileInput = $("#counseling_post_image");
+  fileInput.on("change", showPreview);
+});
+
+
+
+
+// ユーザー編集の画像表示
+$(document).on('turbolinks:load', function() {
+  $("#user-image-preview").hide();
+
+  function showPreview(event) {
+    var file = event.target.files[0];
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      var previewElement = $("#user-image-preview");
+      previewElement.attr("src", e.target.result);
+      $(".user-card-img").hide(); // user-card-imgクラスを持つ要素を非表示
+      previewElement.show();　// 画像を表示
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);  // ファイルを読み込んでData URLに変換
+    }
+  }
+
+  var fileInput = $("#user_profile_image");
   fileInput.on("change", showPreview);
 });
