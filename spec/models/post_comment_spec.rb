2655e63dc664d3f7be2b9abd61a57e9f5bd17e6d
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-06-23 06:27:51 +0000

    [Update]respecテスト UserモデルとPostCommetモデルのバリデーションテスト

--- spec/models/post_comment_spec.rb
+++ spec/models/post_comment_spec.rb
@@ -7,11 +7,41 @@ RSpec.describe PostComment, "モデルに関するテスト", type: :model do
     end
   end
 
-  context '空白のバリデーションチェック' do
-    it 'commentが空白の場合にバリデーションチェックされ、空白のエラーメッセージが返ってきている' do
-      post_comment = PostComment.new(comment: '')
-      expect(post_comment).to be_invalid
-      expect(post_comment.errors[:comment]).to include("を入力してください")
+  describe 'バリデーションのテスト' do
+    subject { post_comment.valid? }
+
+    let(:post_comment) { FactoryBot.build(:post_comment) }
+
+    context '空白のバリデーションチェック' do
+      it 'commentが空白の場合にバリデーションチェックされ、空白のエラーメッセージが返ってきている' do
+        post_comment = PostComment.new(comment: '')
+        expect(post_comment).to be_invalid
+        expect(post_comment.errors[:comment]).to include("を入力してください")
+      end
+    end
+
+    context 'commentカラム' do
+      it '500文字以下であること' do
+        post_comment.comment = Faker::Lorem.characters(number: 500)
+        is_expected.to eq true
+      end
+      it '501文字の場合保存されない' do
+        post_comment.comment = Faker::Lorem.characters(number: 501)
+        is_expected.to eq false
+      end
+    end
+  end
+
+  describe 'アソシエーションのテスト' do
+    context 'userモデルとの関係' do
+      it 'N:1となっている' do
+        expect(PostComment.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+    context 'CounselingPostモデルとの関係' do
+      it 'N:1となっている' do
+        expect(PostComment.reflect_on_association(:counseling_post).macro).to eq :belongs_to
+      end
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,23 +10,62 @@ RSpec.describe User, "モデルに関するテスト", type: :model do
     end
   end
 
-  context "空白のバリデーションチェック" do
-    it "名前が空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
-      user = User.new(name: "", email: "test@example.com", password: "password")
-      expect(user).to be_invalid
-      expect(user.errors[:name]).to include("を入力してください")
+  describe 'バリデーションのテスト' do
+    subject { user.valid? }
+
+    let!(:other_user) { create(:user) }
+    let(:user) { build(:user) }
+
+    context 'nameカラム' do
+      it '空欄でないこと' do
+        user.name = ''
+        is_expected.to eq false
+      end
+      it '15文字以下であること' do
+        user.name = Faker::Lorem.characters(number: 15)
+        is_expected.to eq true
+      end
+      it '16文字の場合保存されない' do
+        user.name = Faker::Lorem.characters(number: 16)
+        is_expected.to eq false
+      end
     end
 
-    it "メールが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
-      user = User.new(name: "Test User", email: "", password: "password")
-      expect(user).to be_invalid
-      expect(user.errors[:email]).to include("を入力してください")
+    context 'introductionカラム' do
+      it '50文字以下であること' do
+        user.introduction = Faker::Lorem.characters(number: 50)
+        is_expected.to eq true
+      end
+      it '51文字の場合保存されない' do
+        user.introduction = Faker::Lorem.characters(number: 51)
+        is_expected.to eq false
+      end
     end
 
-    it "パスワードが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
-      user = User.new(name: "Test User", email: "test@example.com", password: "")
-      expect(user).to be_invalid
-      expect(user.errors[:password]).to include("を入力してください")
+    context "空白のバリデーションチェック" do
+      it "名前が空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
+        user = User.new(name: "", email: "test@example.com", password: "password")
+        expect(user).to be_invalid
+        expect(user.errors[:name]).to include("を入力してください")
+      end
+      it "メールが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
+        user = User.new(name: "Test User", email: "", password: "password")
+        expect(user).to be_invalid
+        expect(user.errors[:email]).to include("を入力してください")
+      end
+      it "パスワードが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
+        user = User.new(name: "Test User", email: "test@example.com", password: "")
+        expect(user).to be_invalid
+        expect(user.errors[:password]).to include("を入力してください")
+      end
+    end
+  end
+
+  describe 'アソシエーションのテスト' do
+    context 'CounselingPostモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:counseling_posts).macro).to eq :has_many
+      end
     end
   end
 end
