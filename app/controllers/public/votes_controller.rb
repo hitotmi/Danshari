ae61e8aa984686396531d850ca9c6e7a7197f2e4
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-07-04 04:03:55 +0000

    [Updata]投票後のページ位置保持のためにアンカーリンクを追加

--- app/controllers/public/votes_controller.rb
+++ app/controllers/public/votes_controller.rb
@@ -7,18 +7,17 @@ class Public::VotesController < ApplicationController
     @vote.option = vote_params[:option]
     # 投票が初めて作成されたものか、既存のものかを判定
     is_new_vote = @vote.new_record?
-  
+
     if @vote.save
       # 投票されたことを、投稿者に通知を作成する
       @counseling_post.create_vote_notification_by(current_user, @counseling_post.id, @counseling_post.user_id, @vote.option, is_new_vote)
-      redirect_to  counseling_post_path(@counseling_post)
-      flash[:notice] = "投票完了しました"
+      redirect_to counseling_post_path(@counseling_post, anchor: 'voting-section')
+      flash[:vote_notice] = "投票完了しました"
     else
       redirect_to  counseling_post_path(@counseling_post)
-      flash[:alert] = "投票に失敗しました。選択しなおしてください。"
+      flash[:vote_alert] = "投票に失敗しました。選択しなおしてください。"
     end
   end
-
 
   private
 
